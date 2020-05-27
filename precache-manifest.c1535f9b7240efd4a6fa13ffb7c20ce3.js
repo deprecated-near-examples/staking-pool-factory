65d336051098df434a52334629881765bfa70f90
Evgeny Kuzyakov <ek@nearprotocol.com>
2020-05-27 15:39:18 -0700

    Updates

---
+++ precache-manifest.c1535f9b7240efd4a6fa13ffb7c20ce3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e1e358d633a78cdf1aeab50827d18c",
+    "url": "/staking-pool-factory/index.html"
+  },
+  {
+    "revision": "b3a15fa0738edf77b828",
+    "url": "/staking-pool-factory/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "54eae99637317fb12b5d",
+    "url": "/staking-pool-factory/static/js/2.14beee9d.chunk.js"
+  },
+  {
+    "revision": "0c37cc6bb2da54e2fe5c4b9e36e115d2",
+    "url": "/staking-pool-factory/static/js/2.14beee9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3a15fa0738edf77b828",
+    "url": "/staking-pool-factory/static/js/main.b893ea83.chunk.js"
+  },
+  {
+    "revision": "485aacc987f2eb4c4062",
+    "url": "/staking-pool-factory/static/js/runtime-main.fb6f3627.js"
+  }
+]);
